a887585d3a620e3b46a228381f6eab4d1b7fb057
Laerkebn <lbnonved@gmail.com>
2025-09-24 15:54:47 +0200

    sætter three op

--- src/main.js
+++ src/main.js
@@ -1,24 +1,7 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'tree'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.scene()
 
-setupCounter(document.querySelector('#counter'))
+const camara = new pespectiveCamara(75, window.innerWidth / innerHeight, 0.1, 1000)
+scene.add(camara)
